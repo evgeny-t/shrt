61e6b897bdd5dcb3d3d0058b9b0e506592127625
etimoshenko <timoshenko.evgeny@gmail.com>
2018-05-13 14:15:25 +0200

    handle events in Shoort

--- src/components/Shoort/index.js
+++ src/components/Shoort/index.js
@@ -1,19 +1,46 @@
 import React from "react";
+import PropTypes from "prop-types";
+import cx from "classnames";
 import "./styles.scss";
 
 export class Shoort extends React.Component {
+  static propTypes = {
+    onShorten: PropTypes.func.isRequired
+  };
+  state = {
+    value: ""
+  };
+
   render() {
     return (
       <div className="shoort">
         <input
+          value={this.state.value}
+          onChange={this._handleChange}
           className="shoort__input"
           type="text"
           placeholder="Paste the link you want to shorten here"
         />
-        <button className="shoort__btn shoort__btn--disabled">
+        <button
+          className={cx("shoort__btn", {
+            "shoort__btn--disabled": !this.state.value
+          })}
+          onClick={this._handleClick}
+        >
           Shorten this link
         </button>
       </div>
     );
   }
+
+  _handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
+  _handleClick = () => {
+    const result = this.props.onShorten(this.state.value);
+    if (result && result.then) {
+      result.then(() => this.setState({ value: "" }));
+    }
+  };
 }
