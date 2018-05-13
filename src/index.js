03c376ebcc79be0cf8a9aeac173fe6dfd72c4ec8
etimoshenko <timoshenko.evgeny@gmail.com>
2018-05-13 14:02:31 +0200

    use App

--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import { create, get, stats } from "./api";
-import { Header, Shoort, History } from "./components";
+import { App } from "./components";
 
-ReactDOM.render(
-  <div>
-    <Header />
-    <Shoort />
-    <History />
-  </div>,
-  document.body
-);
+ReactDOM.render(<App />, document.body);
 
 create("http://example.com")
   .then(({ shortcode }) => stats(shortcode))
