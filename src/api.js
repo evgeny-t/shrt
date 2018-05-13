7729a830df48557f45601f94bd6bbfce3b52e178
etimoshenko <timoshenko.evgeny@gmail.com>
2018-05-13 12:02:27 +0200

    implement api calls

---
+++ src/api.js
@@ -0,0 +1,23 @@
+const pre = "https://us-central1-my-bio-163107.cloudfunctions.net/proxy";
+// const pre = "http://localhost:8010/my-bio-163107/us-central1/proxy";
+
+export const create = url => {
+  return fetch(`${pre}/shorten`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      url
+    })
+  }).then(response => response.json());
+};
+
+export const get = shortcode => {
+  return fetch(`${pre}/${shortcode}`).then(response =>
+    response.headers.get("Location")
+  );
+};
+
+export const stats = shortcode =>
+  fetch(`${pre}/${shortcode}/stats`).then(response => response.json());
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import "./index.scss";
-import { create } from "./api";
+import { create, get, stats } from "./api";
 
-Promise.all([import('react-dom'), import('react')])
-.then(([ReactDOM, React]) => {
-  ReactDOM.render(<div />, document.body);
-});
-window.create = create;
+Promise.all([import("react-dom"), import("react")]).then(
+  ([ReactDOM, React]) => {
+    ReactDOM.render(<div />, document.body);
+  }
+);
+
+create("http://example.com")
+  .then(({ shortcode }) => stats(shortcode))
+  .then(loc => console.log(loc));
